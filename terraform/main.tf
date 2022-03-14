67e53d5d4fdcea971f5c137efaaa58a99baad4ab
Emerson Ferreira <emerson.ferreira@fcamara.com.br>
2022-03-14 00:05:49 -0300

    Adjusting the CI-CD yaml file.

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,49 +9,36 @@ terraform {
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
-  features {}
+  # NOTE: Environment Variables can also be used for Service Principal authentication
+  # Terraform also supports authenticating via the Azure CLI too.
+  # see here for more info: http://terraform.io/docs/providers/azurerm/index.html
+
+  # subscription_id = $AZURE_SUB_ID
+  # client_id       = $AZURE_CLIENT_ID
+  # client_secret   = $AZURE_CLIENT_SECRET
+  # tenant_id       = $AZURE_TENANT_ID
 }
 
+# Create a resource group
 resource "azurerm_resource_group" "servian" {
-  name     = "rg-servian-demo"
+  name     = "rg-servian"
   location = "East US 2"
 }
 
-resource "azurerm_virtual_network" "demo" {
-  name                = "demo-vnet"
+# Create a virtual network in the web_servers resource group
+resource "azurerm_virtual_network" "network" {
+  name                = "production-network"
+  resource_group_name = "${azurerm_resource_group.servian.name}"
+  location            = "${azurerm_resource_group.servian.location}"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.servian.location
-  resource_group_name = azurerm_resource_group.servian.name
-}
 
-resource "azurerm_subnet" "database" {
-  name                 = "db-subnet"
-  resource_group_name  = azurerm_resource_group.servian.name
-  virtual_network_name = azurerm_virtual_network.demo.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-  delegation {
-    name = "delegation"
-
-    service_delegation {
-      name    = "Microsoft.ContainerInstance/containerGroups"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
-    }
+  subnet {
+    name           = "sb-db"
+    address_prefix = "10.0.1.0/24"
   }
-}
 
-resource "azurerm_subnet" "aks" {
-  name                 = "aks-subnet"
-  resource_group_name  = azurerm_resource_group.servian.name
-  virtual_network_name = azurerm_virtual_network.demo.name
-  address_prefixes     = ["10.0.2.0/24"]
-
-  delegation {
-    name = "delegation"
-
-    service_delegation {
-      name    = "Microsoft.ContainerInstance/containerGroups"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
-    }
+  subnet {
+    name           = "sb-aks"
+    address_prefix = "10.0.2.0/24"
   }
 }
