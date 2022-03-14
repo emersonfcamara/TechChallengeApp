430f4504df0357b4e77e0ef66823b58ec4d2caee
Emerson Ferreira <emerson.ferreira@fcamara.com.br>
2022-03-14 00:55:25 -0300

    Adjusting the CI-CD yaml file.

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,26 +16,64 @@ provider "azurerm" {
   features {}
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "servian" {
-  name     = "rg-servian"
-  location = "East US 2"
+resource "azurerm_resource_group" "akc-rg" {
+  name     = "${var.resource_group_name}"
+  location = "${var.resource_group_location}"
 }
 
-# Create a virtual network in the web_servers resource group
-resource "azurerm_virtual_network" "network" {
-  name                = "production-network"
-  resource_group_name = "${azurerm_resource_group.servian.name}"
-  location            = "${azurerm_resource_group.servian.location}"
-  address_space       = ["10.0.0.0/16"]
+#an attempt to keep the aci container group name (and dns label) somewhat unique
+resource "random_integer" "random_int" {
+  min = 100
+  max = 999
+}
 
-  subnet {
-    name           = "sb-db"
-    address_prefix = "10.0.1.0/24"
+resource azurerm_network_security_group "aks_advanced_network" {
+  name                = "akc-${random_integer.random_int.result}-nsg"
+  location            = "${var.resource_group_location}"
+  resource_group_name = "${azurerm_resource_group.akc-rg.name}"
+}
+
+resource "azurerm_virtual_network" "aks_advanced_network" {
+  name                = "akc-${random_integer.random_int.result}-vnet"
+  location            = "${var.resource_group_location}"
+  resource_group_name = "${azurerm_resource_group.akc-rg.name}"
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                      = "akc-${random_integer.random_int.result}-subnet"
+  resource_group_name       = "${azurerm_resource_group.akc-rg.name}"
+  network_security_group_id = "${azurerm_network_security_group.aks_advanced_network.id}"
+  address_prefix            = "10.1.0.0/24"
+  virtual_network_name      = "${azurerm_virtual_network.aks_advanced_network.name}"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_container" {
+  name       = "akc-${random_integer.random_int.result}"
+  location   = "${var.resource_group_location}"
+  dns_prefix = "akc-${random_integer.random_int.result}"
+
+  resource_group_name = "${azurerm_resource_group.akc-rg.name}"
+
+  agent_pool_profile {
+    name    = "agentpool"
+    count   = "2"
+    vm_size = "Standard_DS2_v2"
+    os_type = "Linux"
+
+    # Required for advanced networking
+    vnet_subnet_id = "${azurerm_subnet.aks_subnet.id}"
   }
 
-  subnet {
-    name           = "sb-aks"
-    address_prefix = "10.0.2.0/24"
+  service_principal {
+    client_id     = "${var.client_id}"
+    client_secret = "${var.client_secret}"
+  }
+
+  network_profile {
+    network_plugin     = "azure"
+    dns_service_ip     = "10.0.0.10"
+    docker_bridge_cidr = "172.17.0.1/16"
+    service_cidr       = "10.0.0.0/16"
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,27 @@
-variable "region" {}
-variable "user" {}
-variable "label" {}
-variable "app" {}
+variable "name" {
+  type        = "string"
+  description = "Name of this cluster."
+  default     = "akc-example"
+}
+
+variable "client_id" {
+  type        = "string"
+  description = "Client ID"
+}
+
+variable "client_secret" {
+  type        = "string"
+  description = "Client secret."
+}
+
+variable "resource_group_name" {
+  type        = "string"
+  description = "Name of the azure resource group."
+  default     = "akc-rg"
+}
+
+variable "resource_group_location" {
+  type        = "string"
+  description = "Location of the azure resource group."
+  default     = "eastus"
+}
